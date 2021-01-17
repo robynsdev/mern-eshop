c1e6709e6dea6243866af1ea806dfa73f3122de0
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-17 18:15:16 +1100

    added .env

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const products = require("./data/products")
+const dotenv = require("dotenv");
+const products = require("./data/products");
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
+app.get("/api/products", (req, res) => {
+  res.json(products);
   // converts objects in file to json
-})
+});
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`));
